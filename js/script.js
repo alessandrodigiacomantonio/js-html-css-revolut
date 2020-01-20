48c72829d943e5ee0ef9257d8cc162e32c23f56b
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-20 19:30:45 +0100

    Aggiunti elementi beta e new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('.items').hover( function () {
+    $('#lingua').find('.fa-chevron-down').addClass('fa-chevron-up');
+    $('#lingua').find('.fa-chevron-up').removeClass('fa-chevron-down');
     $(this).find('.items__dropdown').toggle();
   });
 });
