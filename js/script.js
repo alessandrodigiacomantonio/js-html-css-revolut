bf74d1145239bfaa9a01a7f761a73d064ad70003
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-20 19:43:12 +0100

    Fixato chevron

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $('.items').hover( function () {
+    $(this).find('.items__dropdown').toggle();
+  });
+  $('li#lingua.items').hover( function() {
     $('#lingua').find('.fa-chevron-down').addClass('fa-chevron-up');
     $('#lingua').find('.fa-chevron-up').removeClass('fa-chevron-down');
-    $(this).find('.items__dropdown').toggle();
+  }, function() {
+    $('#lingua').find('.fa-chevron-up').addClass('fa-chevron-down');
+    $('#lingua').find('.fa-chevron-down').removeClass('fa-chevron-up');
   });
 });
