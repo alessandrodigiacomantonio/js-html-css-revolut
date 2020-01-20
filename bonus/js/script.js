f31f551da0aa2834d6d60cad041f87689979ae3c
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-21 00:53:21 +0100

    Scritto comportamento chevron di #lingue

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
   $('.header__nav__right .items').hover(
     function () {
-      console.log( !$('#tariffazione') );
-      if ( $(this).siblings('#tariffazione').length == 1 && !$(this).hasClass('active') ) {
+      if ($(this).siblings('#lingua').length == 0) {
         $(this).siblings().removeClass('active');
         $(this).siblings().find('.items__dropdown').hide();
         $(this).addClass('active');
         $(this).find('.items__dropdown').show();
+        $('#lingua').find('.fa-chevron-down').addClass('fa-chevron-up');
+        $('#lingua').find('.fa-chevron-up').removeClass('fa-chevron-down');
+      }
+      if ( $(this).siblings('#tariffazione').length == 1 && !$(this).hasClass('active') && $(this).siblings('#lingua').length == 1) {
+        $(this).siblings().removeClass('active');
+        $(this).siblings().find('.items__dropdown').hide();
+        $(this).addClass('active');
+        $(this).find('.items__dropdown').show();
+        $('#lingua').find('.fa-chevron-up').addClass('fa-chevron-down');
+        $('#lingua').find('.fa-chevron-down').removeClass('fa-chevron-up');
       }
     }, function () {
-      if ( !$('#tariffazione') ) {
-        $(this).find('.items__dropdown').hide();
-      }
+      console.log('A caval donato non si guarda in bocca');
     });
   $('.header__nav__right .items').on('click', function () {
+    if ($(this).siblings('#lingua').length == 0) {
+      if ($(this).hasClass('active') ) {
+        $('#lingua').find('.fa-chevron-up').addClass('fa-chevron-down');
+        $('#lingua').find('.fa-chevron-down').removeClass('fa-chevron-up');
+      } else {
+        $('#lingua').find('.fa-chevron-down').addClass('fa-chevron-up');
+        $('#lingua').find('.fa-chevron-up').removeClass('fa-chevron-down');
+      }
+    }
     if (!$(this).hasClass('active')) {
       $('.header__nav__right .items').removeClass('active');
       $(this).toggleClass('active');
@@ -23,7 +39,7 @@ $(document).ready(function() {
       $(this).find('.items__dropdown').hide();
     }
   });
-  // $('html').on('click', function() {
+  // $(':not(.items)').on('click', function() {
   //   $('.header__nav__right .items').removeClass('active');
   //   $('.header__nav__right .items').find('.items__dropdown').hide();
   // });
