804e9c1177d503e2e0da44f5ecc0718f0bb4e3d2
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-20 23:07:35 +0100

    Scritti stati active e hover della nav nell'header

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
-  $('.header__nav .items').hover( function () {
-
-    if (!$(this).hasClass('active')) {
-      $(this).find('.items__dropdown').show();
-    }
-  });
-  $('li#lingua.items').hover(
+  $('.header__nav__right .items').hover(
+    function () {
+      if ( !$(this).hasClass('active') || !$('#tariffazione') ) {
+        $(this).siblings().removeClass('active');
+        $(this).siblings().find('.items__dropdown').hide();
+        $(this).addClass('active');
+        $(this).find('.items__dropdown').show();
+      }
+    }, function () {
+      if ( !$(this).hasClass('active') || !$('#tariffazione') ) {
+        $(this).find('.items__dropdown').hide();
+      }
+    });
+  $('.header__nav #lingua.items').hover(
     function() {
     $('#lingua').find('.fa-chevron-down').addClass('fa-chevron-up');
     $('#lingua').find('.fa-chevron-up').removeClass('fa-chevron-down');
   }, function() {
-    $('#lingua').find('.fa-chevron-up').addClass('fa-chevron-down');
-    $('#lingua').find('.fa-chevron-down').removeClass('fa-chevron-up');
+    if ($('.header__nav #lingua.items').hasClass('active')) {
+      $('#lingua').find('.fa-chevron-up').addClass('fa-chevron-down');
+      $('#lingua').find('.fa-chevron-down').removeClass('fa-chevron-up');
+    }
   });
   $('.header__nav__right .items').on('click', function () {
     if (!$(this).hasClass('active')) {
       $('.header__nav__right .items').removeClass('active');
       $(this).toggleClass('active');
-    } else $(this).toggleClass('active');
+      $(this).find('.items__dropdown').show();
+    } else {
+      $(this).toggleClass('active');
+      $(this).find('.items__dropdown').hide();
+    }
   });
 });
